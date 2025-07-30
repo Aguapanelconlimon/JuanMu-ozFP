f56ad0afaa1d4228c52b8be5089ba8edb9e73368
Juan Muñoz <juan199xd@gmail.com>
2025-07-30 15:35:39 -0500

    Inicio C#

--- 1. InicioVisualStudio/1. InicioVisualStudio/Program.cs
+++ 1. InicioVisualStudio/1. InicioVisualStudio/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Hola, Juan Muñoz\n");
+            Console.Write("\tBienvenido a la clase de programación");
         }
     }
 }
