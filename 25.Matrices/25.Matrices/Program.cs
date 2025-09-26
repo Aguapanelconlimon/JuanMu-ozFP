287dd5ac4050552fcea17307072766386e4f117e
Aguapanelconlimon <juan199xd@gmail.com>
2025-09-26 15:27:08 -0500

    Matrices

---
+++ 25.Matrices/25.Matrices/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _25.Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arreglos bidimensionales - Matrices
+
+            int[,] números = new int[2, 2]; //rango=fxc=2x2=4
+            string[,] nombres = new string[3, 2];
+            char[,] caracteres = new char[2, 4];
+            bool[,] banderas = new bool[2, 5];
+            object[,] objetos = new object[2, 5];
+
+            //Asignación de valores con f,c
+
+            números[0, 0] = 15;
+            números[0, 1] = 25;
+            números[1, 0] = 32;
+            números[1, 1] = 4;
+            números[1, 1] = 7;
+            //numeros [1,2] = 7; Error por índices fuera de los rangos
+
+            //Recuperar datos almacenados en una posición
+            Console.WriteLine($"Valor almacenado en la posición del índice 1,0: {números[1, 0]}");
+
+            //Declaración e inicialización de matrices con valores por defecto
+
+            string[,] apellido =
+            {
+                {"chica","lopez","Alvarez" },
+                {"Patiño","Ciro","Botero" },
+                {"Duque","Carmona","Castaño" }
+            };
+
+            //Recorrer matrices para almacenar datos
+
+
+            for (int i = 0; i < 3; i++)//ecorrer cada una de las filas
+            {
+                for (int j = 0; j < 2; j++)//recorrer las columnas
+                {
+                    Console.WriteLine($"Digite nombre para almacenarlo en la posición con índices {i},{j}: ");
+                    nombres[i, j] = Console.ReadLine();
+                }
+            }
+
+            //Recorrer para recuperar sus datos
+
+            for (int i = 0; i < nombres.GetLength(0); i++)//.GetLength (0) devuelve el número de filas de la matris
+            {
+                for (int j = 0; j < nombres.GetLength(1); j++)//.Getlength (1) devuelve el número de columnas
+                {
+                    Console.Write($" {nombres[i, j]}   |");
+                }
+            }
+            Console.WriteLine();
+
+            for (char i = 'a'; i <2; i++)
+            {
+                for (char j='a'; j <4; j++)
+                {
+                    Console.Write($"Digite los caracteres para almacenarlos en la posicion de los indices {i},{j}: ");
+                    caracteres[i,j] = Console.ReadLine();
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 26.Matrices2/26.Matrices2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _26.Matrices2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[2, 3];
+            int[,] matrix2 = new int[2, 3];
+            int[,] matrixSuma = new int[2, 3];
+
+            Console.WriteLine($"Ingrese los elementos de la primera matriz: ");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine($"Ingrese los elementos de la segunda matriz: ");
+            for(int i = 0;i < 2; i++)
+            {
+                for(int j = 0;j < 3; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]");
+                    matrix2[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for(int i = 0;i<2;i++)
+            {
+                for (int j=0;j<3;j++)
+                {
+                    matrixSuma[i,j] = matrix[i,j] + matrix2[i,j];
+                }
+            }
+            Console.WriteLine($"La matriz resultante de la suma de las dos matrices es: ");
+            for ( int i = 0; i<2; i++)
+            {
+                for( int j = 0;j<3; j++)
+                {
+                    Console.Write($"{matrixSuma[i,j]}   |");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
