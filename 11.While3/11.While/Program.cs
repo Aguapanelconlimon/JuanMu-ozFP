6e3e790eaf942fb9ea050a337db260080d1bc8d0
Aguapanelconlimon <juan199xd@gmail.com>
2025-10-02 15:18:04 -0500

    Ciclo While

---
+++ 11.While3/11.While/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _11.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int numero;
+
+            Console.WriteLine("Ingrese números enteros positivos para sumar.");
+            Console.WriteLine("Para terminar, ingrese un número negativo.");
+
+            while (true)
+            {
+                Console.Write("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero < 0)
+                {                    
+                    break;
+                }
+
+                suma += numero; 
+            }
+
+            Console.WriteLine($"La suma total de los números ingresados es: {suma}");
+        }
+    }
+}
