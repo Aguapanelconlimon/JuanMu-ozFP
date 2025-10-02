4c26af55ca3ba58e4ffaf3f3a19f759ef0362aed
Aguapanelconlimon <juan199xd@gmail.com>
2025-10-02 15:25:14 -0500

    Ciclo While

---
+++ 12.While4/12.While/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _12.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+            
+            for (int i = 1; i <= 99; i += 2)
+            {
+                suma += i;
+            }
+
+            Console.WriteLine($"La suma de todos los números impares hasta el 99 es: {suma}");
+        }
+    }
+}
