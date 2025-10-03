1315a313114cf60f76b4027107b4c68b53834956
Aguapanelconlimon <juan199xd@gmail.com>
2025-10-03 15:11:34 -0500

    ProgramaciónModular

---
+++ 28. ProgramaciónModular/28. ProgramaciónModular/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _28._ProgramaciónModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            Console.WriteLine();
+            MostrarMensaje("Soy Juan");
+            MostrarMensaje($"Tengo {CalcularEdad()} años");
+            MostrarMensaje($"Tengo {CalcularEdad(2025, 1895)} años");
+        }
+
+        //Modúlo 1 - Procedimiento sin parámetros
+
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido a la Programación Modular");
+            Console.WriteLine("Estoy siendo llamado desde MostrarMensaje()");
+        }
+
+        //Módulo 2 - Procedimiento con parámetros
+
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //Módulo 3 - Función sin parámetros
+
+        static int CalcularEdad()
+        {
+            int edad = 2025 - 1999;
+            return edad;
+        }
+
+        //Módulo 4 - Función con parámetros
+
+        static int CalcularEdad(int añoActual, int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+    }
+}
