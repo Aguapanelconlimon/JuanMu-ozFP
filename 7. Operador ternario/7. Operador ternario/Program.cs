4dcfd41ac759553eb7457da6251309978b549572
Aguapanelconlimon <juan199xd@gmail.com>
2025-10-07 14:26:25 -0500

    Operador ternario

---
+++ 7. Operador ternario/7. Operador ternario/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _7._Operador_ternario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            string resultado = "";
+
+            Console.WriteLine("Ingresa tu edad: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            resultado = (edad >= 18) ? "Eres mayor de edad." : "Eres menor de edad.";
+
+            Console.WriteLine($"{resultado}");
+        }
+    }
+}
