b59197a70eebe30ade8f75a0b7a7cf7374ffde9e
Aguapanelconlimon <juan199xd@gmail.com>
2025-10-17 14:13:28 -0500

    condicionales

---
+++ 3. condicionales/3. condicionales/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _3._condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noches = 0;
+            double precionoche = 0;
+            double subtotal = 0;
+            double total = 0;
+            double descuento = 0;
+
+            Console.WriteLine("Ingrese la cantidad de noches que se hospedara:");
+            noches = int.Parse(Console.ReadLine());
+
+            subtotal = precionoche * noches;
+
+            if (noches > 3)
+            {
+                descuento = subtotal * 0.17;
+            }
+            else
+            {
+                descuento = subtotal * 0.02;
+            }
+
+            total = subtotal - descuento;
+
+            Console.WriteLine($"subtotal: {subtotal}");
+            Console.WriteLine($"Descuento aplicado: {descuento}");
+            Console.WriteLine($"Monto total a pagar: {total}");
+        }
+
+    }
+}
